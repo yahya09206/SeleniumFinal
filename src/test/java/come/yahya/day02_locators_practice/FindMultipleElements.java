4ed9cc7c7f0d47665638ab27ecc99d4091e63cea
Yahya <yahyahussein91@gmail.com>
2023-12-24 14:02:56 -0800

    set up chrome driver

--- src/test/java/come/yahya/day02_locators_practice/FindMultipleElements.java
+++ src/test/java/come/yahya/day02_locators_practice/FindMultipleElements.java
@@ -1,4 +1,15 @@
 package come.yahya.day02_locators_practice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindMultipleElements {
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/");
+    }
 }
