9d6618b329b5a9f3de610d17f3efc8b6c8cdf215
Yahya <yahyahussein91@gmail.com>
2023-12-24 14:25:37 -0800

    getting different attributes of searchbox

--- src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
+++ src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
@@ -11,20 +11,29 @@ public class GetElementAttribute {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://practice.cydeo.com/");
+        //driver.get("https://practice.cydeo.com/");
 
         // Get the first link on the page and get href attribute
-        WebElement firstLink = driver.findElement(By.linkText("A/B Testing"));
-        // Get any attribute of identified element, in this case the href
-        System.out.println("firstLink.getAttribute(\"href\") = " + firstLink.getAttribute("href"));
-
-        // Get text practice
-        System.out.println("firstLink.getText() = " + firstLink.getText());
+//        WebElement firstLink = driver.findElement(By.linkText("A/B Testing"));
+//        // Get any attribute of identified element, in this case the href
+//        System.out.println("firstLink.getAttribute(\"href\") = " + firstLink.getAttribute("href"));
+//
+//        // Get text practice
+//        System.out.println("firstLink.getText() = " + firstLink.getText());
 
         // Navigate to google.com and identify the searchbox and get couple of attributes value
         driver.navigate().to("https://google.com");
         WebElement searchBox = driver.findElement(By.id("input"));
         searchBox.sendKeys("Selenium");
+
+        // Get the class Atrribute
+        System.out.println("searchBox.getAttribute(\"class\") = " + searchBox.getAttribute("class"));
+        // maxLength
+        System.out.println("searchBox.getAttribute(\"maxLength\") = " + searchBox.getAttribute("maxLength"));
+        // title
+        // autocapitalize
+        // spellcheck
+        // role
 
         Thread.sleep(3000);
         driver.quit();
