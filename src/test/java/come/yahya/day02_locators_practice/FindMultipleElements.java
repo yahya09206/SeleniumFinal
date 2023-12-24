49073b263dc5acf1d0b8a06174148c378f33cbe1
Yahya <yahyahussein91@gmail.com>
2023-12-24 14:04:30 -0800

    created list of web elements

--- src/test/java/come/yahya/day02_locators_practice/FindMultipleElements.java
+++ src/test/java/come/yahya/day02_locators_practice/FindMultipleElements.java
@@ -1,8 +1,12 @@
 package come.yahya.day02_locators_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class FindMultipleElements {
     public static void main(String[] args) {
@@ -13,6 +17,7 @@ public class FindMultipleElements {
         driver.get("https://practice.cydeo.com/");
 
         // Get all the links that have partial text A in it
+        List<WebElement> allLink = driver.findElements(By.partialLinkText("A"));
 
     }
 }
