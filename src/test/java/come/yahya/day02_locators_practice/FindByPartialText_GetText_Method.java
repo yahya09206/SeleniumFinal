06e72dbbb9e719e0d99d4e7622029c68e5e9dae6
Yahya <yahyahussein91@gmail.com>
2023-12-24 13:55:01 -0800

    finding partial link starting with disappear

--- src/test/java/come/yahya/day02_locators_practice/FindByPartialText_GetText_Method.java
+++ src/test/java/come/yahya/day02_locators_practice/FindByPartialText_GetText_Method.java
@@ -1,7 +1,9 @@
 package come.yahya.day02_locators_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindByPartialText_GetText_Method {
@@ -11,6 +13,8 @@ public class FindByPartialText_GetText_Method {
         WebDriver driver = new ChromeDriver();
         driver.get("https://practice.cydeo.com/");
 
-
+        // Identify the link with text "Disappearing Elements"
+        // Using partial link text then get the full text out using new method getText
+        WebElement myLnk = driver.findElement(By.partialLinkText("Disappearing"));
     }
 }
