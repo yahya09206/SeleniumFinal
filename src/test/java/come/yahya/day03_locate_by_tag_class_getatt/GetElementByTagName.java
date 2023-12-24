17e58094fb61d82144d342968d210d37ea5d29b0
Yahya <yahyahussein91@gmail.com>
2023-12-24 14:40:23 -0800

    driver was pointing to wrong site

--- src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementByTagName.java
+++ src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementByTagName.java
@@ -6,17 +6,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class GetElementByTagName {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Setup Chromedriver
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        driver.get("https://practice.cydeo.com/");
 
         // Identify first element with tag name <a>
         WebElement firstLink = driver.findElement(By.tagName("a"));
         // Now lets get the text to find out
         System.out.println("firstLink.getText() = " + firstLink.getText());
+
+        // Get all elements with the "a" tag
+        List<WebElement> allATags = driver.findElements(By.tagName("a"));
+        System.out.println("allATags.size() = " + allATags.size());
+
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
