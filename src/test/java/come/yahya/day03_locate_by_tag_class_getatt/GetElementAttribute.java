88635aec99bae974a631bd6cfb94fe61207ce761
Yahya <yahyahussein91@gmail.com>
2023-12-24 14:16:32 -0800

    getting attribute of href for link provided

--- src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
+++ src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
@@ -1,7 +1,9 @@
 package come.yahya.day03_locate_by_tag_class_getatt;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GetElementAttribute {
@@ -10,5 +12,16 @@ public class GetElementAttribute {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://practice.cydeo.com/");
+
+        // Get the first link on the page and get href attribute
+        WebElement firstLink = driver.findElement(By.linkText("A/B Testing"));
+        // Get any attribute of identified element, in this case the href
+        System.out.println("firstLink.getAttribute(\"href\") = " + firstLink.getAttribute("href"));
+
+        // Get text practice
+        System.out.println("firstLink.getText() = " + firstLink.getText());
+
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
