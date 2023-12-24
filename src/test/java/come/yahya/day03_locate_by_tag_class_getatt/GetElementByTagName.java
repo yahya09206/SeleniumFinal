a4f33e1699b2aa8e417ca954cdb497c9585da8f7
Yahya <yahyahussein91@gmail.com>
2023-12-24 14:37:07 -0800

    printing out first link that contains text a

--- src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementByTagName.java
+++ src/test/java/come/yahya/day03_locate_by_tag_class_getatt/GetElementByTagName.java
@@ -1,7 +1,9 @@
 package come.yahya.day03_locate_by_tag_class_getatt;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GetElementByTagName {
@@ -13,5 +15,8 @@ public class GetElementByTagName {
         driver.get("https://www.google.com/");
 
         // Identify first element with tag name <a>
+        WebElement firstLink = driver.findElement(By.tagName("a"));
+        // Now lets get the text to find out
+        System.out.println("firstLink.getText() = " + firstLink.getText());
     }
 }
