c055e0f9d5f0b53a7bf15cfae021fdb7c4103e05
Yahya <yahyahussein91@gmail.com>
2023-12-27 14:25:10 -0800

    selecting first raido button

--- src/test/java/come/yahya/day04_common_elements_xpath_css_intro/LocatingRadioButtons.java
+++ src/test/java/come/yahya/day04_common_elements_xpath_css_intro/LocatingRadioButtons.java
@@ -1,14 +1,23 @@
 package come.yahya.day04_common_elements_xpath_css_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatingRadioButtons {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://practice.cydeo.com/radiobuttons");
+
+        // Locate the blue radio button
+        WebElement blueRadio = driver.findElement(By.id("blue"));
+        System.out.println("blueRadio.isSelected() = " + blueRadio.isSelected());
+
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
