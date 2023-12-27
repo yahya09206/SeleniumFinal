cd79134664d123cdd30c0a69114cf62d5af09863
Yahya <yahyahussein91@gmail.com>
2023-12-27 14:06:26 -0800

    set up web driver and navigated to site

--- src/test/java/come/yahya/day04_common_elements_xpath_css_intro/LocatingCheckBoxes.java
+++ src/test/java/come/yahya/day04_common_elements_xpath_css_intro/LocatingCheckBoxes.java
@@ -1,8 +1,18 @@
 package come.yahya.day04_common_elements_xpath_css_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LocatingCheckBoxes {
     public static void main(String[] args) {
 
-        // Checkboxes
+        // Checkboxes can be checked and unchecked
+        // That's the only attribute special about the element
+        // We can use additional method called isSelected() to determine if it's checked
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/checkboxes");
     }
 }
