ce5100a54c2c40f52d094546558ff83e39bb9da4
Yahya <yahyahussein91@gmail.com>
2023-12-27 14:50:35 -0800

    locating dropdown lists

---
+++ src/test/java/come/yahya/day04_common_elements_xpath_css_intro/LocatingDropdownList.java
@@ -0,0 +1,15 @@
+package come.yahya.day04_common_elements_xpath_css_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatingDropdownList {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/dropdown");
+
+    }
+}
