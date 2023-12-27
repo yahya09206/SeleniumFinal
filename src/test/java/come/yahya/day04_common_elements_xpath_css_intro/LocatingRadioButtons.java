931a777d24a97c39a363674f54ac6a74856a57c3
Yahya <yahyahussein91@gmail.com>
2023-12-27 14:20:05 -0800

    selecting radio buttons

---
+++ src/test/java/come/yahya/day04_common_elements_xpath_css_intro/LocatingRadioButtons.java
@@ -0,0 +1,14 @@
+package come.yahya.day04_common_elements_xpath_css_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatingRadioButtons {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/radiobuttons");
+    }
+}
