0c249b38e9780fd95c22ff32f66b3f9277eaa58a
Yahya <yahyahussein91@gmail.com>
2023-12-28 08:46:03 -0800

    messing around with different xpath combos

--- src/test/java/come/yahya/day04_common_elements_xpath_css_intro/FindElementsByXpath_Intro.java
+++ src/test/java/come/yahya/day04_common_elements_xpath_css_intro/FindElementsByXpath_Intro.java
@@ -21,7 +21,17 @@ public class FindElementsByXpath_Intro {
 
         // Using relative path
         WebElement headerSpan2 = driver.findElement(By.xpath("//h1/span"));
-        System.out.println("headerSpan2 = " + headerSpan2);
+        System.out.println("headerSpan2.getText() = " + headerSpan2.getText());
+
+        // Providing attributes in xpath
+        // elementTag[@attributeName='attribute value']
+        // if you have more than one
+        // elementTag[@attributeName='attribute value' and @attribute2Name='attribute2 value']
+        // you have option to provide idnex
+        // //someElement[indexGoesHere]
+        // /html/body/div/div[2]/div/form/span[1]
+        // //*[@id="checkboxes"]/span[1]
+        // //*[@id="box1"]
 
         Thread.sleep(3000);
         driver.quit();
