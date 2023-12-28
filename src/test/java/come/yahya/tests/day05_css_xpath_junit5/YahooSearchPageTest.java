0e1ebf3d76d6b4fae2ae7b094189e79a46f49872
Yahya <yahyahussein91@gmail.com>
2023-12-28 10:32:06 -0800

    assertion is returning correct result

--- src/test/java/come/yahya/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/come/yahya/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
@@ -1,6 +1,7 @@
 package come.yahya.tests.day05_css_xpath_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,11 +13,19 @@ public class YahooSearchPageTest {
     // Test when you navigate to yahoo search page, the title should be "Yahoo Search - Web Search
 
     @Test
-    public void testYahooSearchResultPagetitle(){
+    public void testYahooSearchResultPagetitle() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://search.yahoo.com/");
+
+        String expectedTitle = "Yahoo Search - Web Search";
+        String actualTitle = driver.getTitle();
+
+        Assertions.assertEquals(expectedTitle, actualTitle);
+
+        Thread.sleep(3000);
+        driver.quit();
 
 
     }
