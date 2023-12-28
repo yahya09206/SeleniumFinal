b702129dec002fce253f5ff07e691a099b77c6aa
Yahya <yahyahussein91@gmail.com>
2023-12-28 10:13:42 -0800

    first junit test

---
+++ src/test/java/come/yahya/tests/day05_css_xpath_junit5/FirstTest.java
@@ -0,0 +1,11 @@
+package come.yahya.tests.day05_css_xpath_junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    
+    @Test
+    public void testAddition(){
+        
+    }
+}
