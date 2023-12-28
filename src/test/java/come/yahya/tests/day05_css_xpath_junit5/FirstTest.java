969d1856ab987d9afb581d2d8598170f5d8b8442
Yahya <yahyahussein91@gmail.com>
2023-12-28 10:16:32 -0800

    applied assertion

--- src/test/java/come/yahya/tests/day05_css_xpath_junit5/FirstTest.java
+++ src/test/java/come/yahya/tests/day05_css_xpath_junit5/FirstTest.java
@@ -1,11 +1,16 @@
 package come.yahya.tests.day05_css_xpath_junit5;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
     
     @Test
     public void testAddition(){
-        
+
+        int num1 = 10;
+        int num2 = 7;
+        int expectedResult = 17;
+        Assertions.assertEquals(expectedResult, num1 + num2);
     }
 }
