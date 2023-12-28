c8b6ddffc07c9c188c8f0d124ba8b1aa7209a28a
Yahya <yahyahussein91@gmail.com>
2023-12-28 09:07:26 -0800

    selecting email box in different ways

--- src/test/java/come/yahya/day05_css_xpath_junit5/CssSelectorPractice1.java
+++ src/test/java/come/yahya/day05_css_xpath_junit5/CssSelectorPractice1.java
@@ -1,7 +1,9 @@
 package come.yahya.day05_css_xpath_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CssSelectorPractice1 {
@@ -12,6 +14,10 @@ public class CssSelectorPractice1 {
         driver.get("https://practice.cydeo.com/forgot_password");
 
         // Locate 2 elements using css selector
+        // input[name='email']
+        // form#forgot_password input[name='email']
+        WebElement emailBox = driver.findElement(By.cssSelector("input[name='email']"));
+        emailBox.sendKeys("someone@somewhere.com");
 
     }
 }
