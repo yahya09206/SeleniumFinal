8d3ca2b20cacc3a3a837cd944c7e6f35a51e1961
Yahya <yahyahussein91@gmail.com>
2023-12-28 09:11:31 -0800

    printing out confirmation message

--- src/test/java/come/yahya/day05_css_xpath_junit5/CssSelectorPractice1.java
+++ src/test/java/come/yahya/day05_css_xpath_junit5/CssSelectorPractice1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CssSelectorPractice1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -18,6 +18,16 @@ public class CssSelectorPractice1 {
         // form#forgot_password input[name='email']
         WebElement emailBox = driver.findElement(By.cssSelector("input[name='email']"));
         emailBox.sendKeys("someone@somewhere.com");
+
+        WebElement retrieveBtn = driver.findElement(By.cssSelector("#form_submit"));
+        retrieveBtn.click();
+
+        // Identify the success message and print out the text
+        WebElement confirmationMsg = driver.findElement(By.cssSelector("div>h4[name='confirmation_message']"));
+        System.out.println("confirmationMsg.getText() = " + confirmationMsg.getText());
+
+        Thread.sleep(3000);
+        driver.quit();
 
     }
 }
