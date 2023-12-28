95af7973abc091b6697bee93f08ca52a427e4d09
Yahya <yahyahussein91@gmail.com>
2023-12-28 10:49:17 -0800

    setting up webdriver using before annotation for less code

--- src/test/java/come/yahya/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/come/yahya/tests/day05_css_xpath_junit5/YahooSearchPageTest.java
@@ -2,6 +2,8 @@ package come.yahya.tests.day05_css_xpath_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -11,16 +13,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YahooSearchPageTest {
 
+    WebDriver driver;
+
+    @BeforeAll
+    public static void setUpDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public static void setupWebDriver(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://search.yahoo.com/");
+    }
+
     // Write 2 tests
     // testYahooSearchPageTitle
     // Test when you navigate to yahoo search page, the title should be "Yahoo Search - Web Search
 
     @Test
     public void testYahooSearchResultPagetitle() throws InterruptedException {
-
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://search.yahoo.com/");
 
         String expectedTitle = "Yahoo Search - Web Search";
         String actualTitle = driver.getTitle();
