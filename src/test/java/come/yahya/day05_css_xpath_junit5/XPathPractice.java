ffdee215c91b607a5766bc2469b5e3ae3e1821e2
Yahya <yahyahussein91@gmail.com>
2023-12-28 09:28:32 -0800

    more xpath practice

---
+++ src/test/java/come/yahya/day05_css_xpath_junit5/XPathPractice.java
@@ -0,0 +1,14 @@
+package come.yahya.day05_css_xpath_junit5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPathPractice {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/forgot_password");
+    }
+}
