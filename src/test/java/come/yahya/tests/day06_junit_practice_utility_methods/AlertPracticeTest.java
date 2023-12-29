5322a8843b5514036013a96b7257470d3a549a1d
Yahya <yahyahussein91@gmail.com>
2023-12-29 14:43:00 -0800

    setting up to use alert methods

--- src/test/java/come/yahya/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
+++ src/test/java/come/yahya/tests/day06_junit_practice_utility_methods/AlertPracticeTest.java
@@ -2,6 +2,7 @@ package come.yahya.tests.day06_junit_practice_utility_methods;
 
 import come.yahya.utility.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 public class AlertPracticeTest extends TestBase {
 
@@ -9,5 +10,18 @@ public class AlertPracticeTest extends TestBase {
     public void testJavaScriptAlert(){
 
         driver.get("https://practice.cydeo.com/javascript_alerts");
+
+        // Alert interface from Selenium is used to deal with alerts
+        // Your driver object can switchTo the alert and take action on it
+        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+        // Driver first needs to switch to the alert box since it's not part of html
+        // using switchTo() method followed by alert() method
+        // .alert() method returns a type Alert from Selenium
+        // and it has 4 methods
+            // accept() --> click on ok button
+            // dismiss() --> click on cancel button
+            // sendKeys(yourText) --> enter text into prompt
+        driver.switchTo().alert().accept();
+
     }
 }
