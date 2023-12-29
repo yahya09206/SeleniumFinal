f85b8c332849bbfd20165ecae35eb27cc2ce2e17
Yahya <yahyahussein91@gmail.com>
2023-12-29 14:24:24 -0800

    added an implicit wait

--- src/test/java/come/yahya/tests/day06_junit_practice_utility_methods/SlowElementTest.java
+++ src/test/java/come/yahya/tests/day06_junit_practice_utility_methods/SlowElementTest.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.TimeUnit;
+
 public class SlowElementTest extends TestBase {
 
     @Test
-    public void testWait(){
+    public void testWait() throws InterruptedException {
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.navigate().to("https://practice.cydeo.com/dynamic_controls");
         driver.findElement(By.xpath("//button[.='Remove']")).click();
 
+        Thread.sleep(3000);
         WebElement itsGoneElm = driver.findElement(By.id("message"));
         System.out.println("itsGoneElm = " + itsGoneElm);
     }
