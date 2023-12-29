64d24e48a2fc705270b4b3ec0ba4633713a33dca
Yahya <yahyahussein91@gmail.com>
2023-12-29 14:19:35 -0800

    test for setting wait

--- src/test/java/come/yahya/tests/day06_junit_practice_utility_methods/SlowElementTest.java
+++ src/test/java/come/yahya/tests/day06_junit_practice_utility_methods/SlowElementTest.java
@@ -1,6 +1,19 @@
 package come.yahya.tests.day06_junit_practice_utility_methods;
 
 import come.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class SlowElementTest extends TestBase {
+
+    @Test
+    public void testWait(){
+
+        driver.navigate().to("https://practice.cydeo.com/dynamic_controls");
+        driver.findElement(By.xpath("//button[.='Remove']")).click();
+
+        WebElement itsGoneElm = driver.findElement(By.id("message"));
+        System.out.println("itsGoneElm = " + itsGoneElm);
+    }
 }
