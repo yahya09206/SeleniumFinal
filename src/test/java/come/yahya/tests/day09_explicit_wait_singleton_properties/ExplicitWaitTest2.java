a7db40a1b41695d1bd873e01384e2f6621da6a01
Yahya <yahyahussein91@gmail.com>
2024-01-05 08:37:45 -0800

    locating button by css selector

--- src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest2.java
+++ src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest2.java
@@ -1,11 +1,26 @@
 package come.yahya.tests.day09_explicit_wait_singleton_properties;
 
+import come.yahya.utility.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
-public class ExplicitWaitTest2 {
+import java.time.Duration;
+
+public class ExplicitWaitTest2 extends TestBase {
 
     @Test
     public void waitUntilElementInteractive(){
 
+        driver.get("https://practice.cydeo.com/dynamic_controls");
+        // elementToBeClickable(By locator)
+        // elementToBeClickable(WebElement element)
+        // An expectation for checking an element is visible and enabled such that you can click it
+
+        // click on enable button and wait until the input = Box is enabled clickable
+        driver.findElement(By.xpath("//button[.='Enable']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(elementToBeClickable(By.cssSelector("form#input-example>input")));
     }
 }
