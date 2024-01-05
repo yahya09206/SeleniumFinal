3145822f600067482e5ff2d9c6cc14d1691713ab
Yahya <yahyahussein91@gmail.com>
2024-01-05 08:27:18 -0800

    more explicit wait testing

---
+++ src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest2.java
@@ -0,0 +1,11 @@
+package come.yahya.tests.day09_explicit_wait_singleton_properties;
+
+import org.junit.jupiter.api.Test;
+
+public class ExplicitWaitTest2 {
+
+    @Test
+    public void waitUntilElementInteractive(){
+
+    }
+}
