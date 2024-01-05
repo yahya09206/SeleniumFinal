b823817b1a4ccc7689b1c25d388475d9c496eb4d
Yahya <yahyahussein91@gmail.com>
2024-01-05 07:54:51 -0800

    first method to navigate to site

--- src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest.java
+++ src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/ExplicitWaitTest.java
@@ -1,8 +1,13 @@
 package come.yahya.tests.day09_explicit_wait_singleton_properties;
 
 import come.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
 
 public class ExplicitWaitTest extends TestBase {
 
-
+    // 1. Navigate to https://practice.cydeo.com/dynamic_loading
+    @Test
+    public void testWaitForTitle(){
+        driver.get("https://practice.cydeo.com/dynamic_loading");
+    }
 }
