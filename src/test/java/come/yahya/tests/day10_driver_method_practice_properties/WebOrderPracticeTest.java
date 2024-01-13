af71ee54feb27bb7868d5676ed94cc823ab652ca
Yahya <yahyahussein91@gmail.com>
2024-01-13 09:56:10 -0800

    checking if title matches

--- src/test/java/come/yahya/tests/day10_driver_method_practice_properties/WebOrderPracticeTest.java
+++ src/test/java/come/yahya/tests/day10_driver_method_practice_properties/WebOrderPracticeTest.java
@@ -1,8 +1,10 @@
 package come.yahya.tests.day10_driver_method_practice_properties;
 
 import come.yahya.utility.BrowserUtil;
+import come.yahya.utility.Driver;
 import come.yahya.utility.TestBase;
 import come.yahya.utility.WebOrderUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
@@ -12,6 +14,10 @@ public class WebOrderPracticeTest extends TestBase {
     public void testDriverPractice(){
 
         WebOrderUtil.openWebOrderapp();
+        // Assert the page title as Web Orders Login to ensure you are at the login page
+        // Driver.getDriver().getTitle();
+        Assertions.assertEquals("Web Orders Login", driver.getTitle());
+
         WebOrderUtil.login("Tester", "test");
 
         BrowserUtil.waitFor(3);
