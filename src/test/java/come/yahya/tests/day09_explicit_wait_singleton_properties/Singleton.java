842fec59a0e276253d3f9fe9045b7870b6683bf1
Yahya <yahyahussein91@gmail.com>
2024-01-13 09:09:37 -0800

    updated description

--- src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/Singleton.java
+++ src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/Singleton.java
@@ -3,6 +3,15 @@ package come.yahya.tests.day09_explicit_wait_singleton_properties;
 /**
  * We want this class to only create one object
  * We want to block creation of more than one object
+
+ * Singleton is a design pattern to have a class only generate one object
+ * in order to achieve Singleton patten
+ * 1. private constructor
+ * 2. private static field with data type same as class
+ * 3. public getter method for private field
+ *      to check if you have already created object
+ *      if not -- create object for the first time
+ *      if yes -- just use existing object
  */
 public class Singleton {
 
