72a149047e687bf906f49dab85323d0093dd9bb1
Yahya <yahyahussein91@gmail.com>
2024-01-13 09:53:56 -0800

    practicing with single driver class

--- src/test/java/come/yahya/tests/day10_driver_method_practice_properties/WebOrderPracticeTest.java
+++ src/test/java/come/yahya/tests/day10_driver_method_practice_properties/WebOrderPracticeTest.java
@@ -1,12 +1,19 @@
 package come.yahya.tests.day10_driver_method_practice_properties;
 
+import come.yahya.utility.BrowserUtil;
 import come.yahya.utility.TestBase;
+import come.yahya.utility.WebOrderUtil;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
 public class WebOrderPracticeTest extends TestBase {
 
     @Test
     public void testDriverPractice(){
 
+        WebOrderUtil.openWebOrderapp();
+        WebOrderUtil.login("Tester", "test");
+
+        BrowserUtil.waitFor(3);
     }
 }
---
+++ src/test/java/come/yahya/utility/WebOrderUtil.java
@@ -0,0 +1,41 @@
+package come.yahya.utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class WebOrderUtil {
+
+    /**
+     * Simply navigate to WebOrderApp
+     */
+    public static void openWebOrderapp(){
+
+        Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+    }
+
+    public static void login(){
+
+        // WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        // Enter username
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        // Enter password
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+        // Click login
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_login_button")).click();
+    }
+
+    public static void login(String username, String password){
+
+        // WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        // Enter username
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_username")).sendKeys(username);
+        // Enter password
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_password")).sendKeys(password);
+        // Click login
+        Driver.getDriver().findElement(By.id("ctl00_MainContent_login_button")).click();
+    }
+
+
+}
