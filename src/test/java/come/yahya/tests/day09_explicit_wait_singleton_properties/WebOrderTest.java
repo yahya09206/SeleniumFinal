692ae965b69806e7bfec078ef2f64a5e7064d89c
Yahya <yahyahussein91@gmail.com>
2024-01-13 08:31:35 -0800

    changed instance of driver to only be called once each time

--- src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/WebOrderTest.java
+++ src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/WebOrderTest.java
@@ -1,6 +1,8 @@
 package come.yahya.tests.day09_explicit_wait_singleton_properties;
 
+import come.yahya.utility.BrowserUtil;
 import come.yahya.utility.TestBase;
+import come.yahya.utility.WebOrderUtility;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -11,11 +13,20 @@ public class WebOrderTest extends TestBase {
 
         driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
 
-        // Enter username
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-        // Enter password
-        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
-        // Click login
-        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+//        // Enter username
+//        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+//        // Enter password
+//        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+//        // Click login
+//        driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+
+
+        WebOrderUtility.login(driver);
+
+        BrowserUtil.waitFor(3);
+
+        // Create a class called WebOrderUtility
+        // Create a static void method called
+        // login(username, password) accept 2 strings for usernamd and password
     }
 }
---
+++ src/test/java/come/yahya/utility/WebOrderUtility.java
@@ -0,0 +1,22 @@
+package come.yahya.utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+// This isn't a test class so we can't extend testbase
+// It simply doesn't make sense
+public class WebOrderUtility {
+
+    public static void login(WebDriver driverParam){
+
+        // WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        // Enter username
+        driverParam.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        // Enter password
+        driverParam.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+        // Click login
+        driverParam.findElement(By.id("ctl00_MainContent_login_button")).click();
+    }
+}
