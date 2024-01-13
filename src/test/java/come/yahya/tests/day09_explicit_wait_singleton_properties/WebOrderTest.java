5e603c9f928673870383fc27b523ef3389c40fdf
Yahya <yahyahussein91@gmail.com>
2024-01-13 08:18:00 -0800

    adding username

--- src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/WebOrderTest.java
+++ src/test/java/come/yahya/tests/day09_explicit_wait_singleton_properties/WebOrderTest.java
@@ -1,8 +1,19 @@
 package come.yahya.tests.day09_explicit_wait_singleton_properties;
 
 import come.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 public class WebOrderTest extends TestBase {
 
+    @Test
+    public void testLogin(){
 
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+
+        // Enter username
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        // Enter password
+        // Click login
+    }
 }
