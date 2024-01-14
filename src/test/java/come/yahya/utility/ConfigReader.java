277e6507cf49ecf81467beae2f05fa72c2608520
Yahya <yahyahussein91@gmail.com>
2024-01-14 15:19:37 -0800

    creating configreader class so file can only be loaded once

---
+++ src/test/java/come/yahya/utility/ConfigReader.java
@@ -0,0 +1,11 @@
+package come.yahya.utility;
+
+import java.util.Properties;
+
+public class ConfigReader {
+
+    // Declare Properties object at class level so it can be accessible in static method
+    private static Properties properties = new Properties();
+
+    // we want to only load the file once, so we can use static block
+}
