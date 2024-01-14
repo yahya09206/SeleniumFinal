e033fb6d61df10b883fa3f52fa9f3da2a37b1b80
Yahya <yahyahussein91@gmail.com>
2024-01-14 15:29:50 -0800

    successfully reading from file

--- src/test/java/come/yahya/tests/day10_driver_method_practice_properties/SeleniumWithPropertiesTest.java
+++ src/test/java/come/yahya/tests/day10_driver_method_practice_properties/SeleniumWithPropertiesTest.java
@@ -1,7 +1,6 @@
 package come.yahya.tests.day10_driver_method_practice_properties;
 
-import come.yahya.utility.Driver;
-import come.yahya.utility.TestBase;
+import come.yahya.utility.*;
 import org.junit.jupiter.api.Test;
 
 public class SeleniumWithPropertiesTest extends TestBase {
@@ -10,7 +9,18 @@ public class SeleniumWithPropertiesTest extends TestBase {
     public void testWebOrderLogin(){
 
         // use ConfigReader everywhere
+        String url = ConfigReader.read("weborder_url");
+        String username = ConfigReader.read("weborder_username");
+        String password = ConfigReader.read("weborder_password");
+
+        System.out.println("url = " + url);
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+
         // navigate by passing value you got from properties test
-        Driver.getDriver().get("");
+        Driver.getDriver().get(url);
+        WebOrderUtil.login(username, password);
+
+        System.out.println("END");
     }
 }
