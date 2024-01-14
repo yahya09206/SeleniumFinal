426539d6643a804c82804ab9ce4b425357c8305b
Yahya <yahyahussein91@gmail.com>
2024-01-14 15:21:09 -0800

    method for reading from file

--- src/test/java/come/yahya/utility/ConfigReader.java
+++ src/test/java/come/yahya/utility/ConfigReader.java
@@ -1,5 +1,7 @@
 package come.yahya.utility;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
@@ -8,4 +10,23 @@ public class ConfigReader {
     private static Properties properties = new Properties();
 
     // we want to only load the file once, so we can use static block
+    static {
+        try {
+            FileInputStream input = new FileInputStream("config.properties");
+            properties.load(input);
+            // this will throw IOException, its parent of FileNotFoundException
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public static String read(String key){
+        return properties.getProperty(key);
+    }
 }
