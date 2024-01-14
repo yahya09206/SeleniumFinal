074a695bc34c88b901f44c586ca93f7231ecccef
Yahya <yahyahussein91@gmail.com>
2024-01-14 15:01:15 -0800

    set up input stream to read from file

--- src/test/java/come/yahya/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
+++ src/test/java/come/yahya/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
@@ -2,10 +2,15 @@ package come.yahya.tests.day10_driver_method_practice_properties;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class PropertyFileReadTest {
 
     @Test
-    public void testInitialRead(){
+    public void testInitialRead() throws FileNotFoundException {
 
+        // Open a connection to the file using FileInputStream object
+        FileInputStream in = new FileInputStream("config.properties");
     }
 }
