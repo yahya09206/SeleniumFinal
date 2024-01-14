70cd6046a285cf7ef6c6a1a21bb1cd91dfc924ab
Yahya <yahyahussein91@gmail.com>
2024-01-14 15:02:47 -0800

    added ioexception to method signature

--- src/test/java/come/yahya/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
+++ src/test/java/come/yahya/tests/day10_driver_method_practice_properties/PropertyFileReadTest.java
@@ -4,13 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PropertyFileReadTest {
 
     @Test
-    public void testInitialRead() throws FileNotFoundException {
+    public void testInitialRead() throws IOException {
 
         // Open a connection to the file using FileInputStream object
         FileInputStream in = new FileInputStream("config.properties");
+
+        // Create empty properties object
+        Properties properties = new Properties();
+
+        // Load the FileInputStream into the Properties Object
+        properties.load(in);
     }
 }
