c60a16197cf97b19c98bcf8a12487b4656aed247
Yahya <yahyahussein91@gmail.com>
2024-01-14 17:37:53 -0800

    creating page for common items

---
+++ src/test/java/come/yahya/pages/WCommonArea.java
@@ -0,0 +1,11 @@
+package come.yahya.pages;
+
+import org.openqa.selenium.WebElement;
+
+public class WCommonArea {
+
+    public WebElement logoutLink;
+    public WebElement viewAllOrderTab;
+    public WebElement viewAllProductsTab;
+    public WebElement orderTab;
+}
