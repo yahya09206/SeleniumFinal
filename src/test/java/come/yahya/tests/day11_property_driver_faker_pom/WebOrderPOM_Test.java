670c7c19235ffc0b76032c21320e1501aeda59f6
Yahya <yahyahussein91@gmail.com>
2024-01-14 16:16:01 -0800

    wrote testing using pom

---
+++ src/test/java/come/yahya/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -0,0 +1,23 @@
+package come.yahya.tests.day11_property_driver_faker_pom;
+
+import come.yahya.pages.WLoginPage;
+import come.yahya.utility.BrowserUtil;
+import come.yahya.utility.Driver;
+import come.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class WebOrderPOM_Test extends TestBase {
+
+    @Test
+    public void testWithPOM_for_login(){
+
+        Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+        WLoginPage loginPage = new WLoginPage();
+
+        loginPage.userNameField.sendKeys("Tester");
+        loginPage.passwordField.sendKeys("test");
+        loginPage.loginButton.click();
+
+        BrowserUtil.waitFor(4);
+    }
+}
