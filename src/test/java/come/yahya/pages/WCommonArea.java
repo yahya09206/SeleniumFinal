c0b1fd8da3901e4474817d8e8cb0d907ff12916c
Yahya <yahyahussein91@gmail.com>
2024-01-14 17:40:44 -0800

    set up elements for common are pages

--- src/test/java/come/yahya/pages/WCommonArea.java
+++ src/test/java/come/yahya/pages/WCommonArea.java
@@ -1,11 +1,22 @@
 package come.yahya.pages;
 
+import come.yahya.utility.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class WCommonArea {
 
+    @FindBy(linkText = "ctl00_logout")
     public WebElement logoutLink;
+    @FindBy(linkText = "View all Orders")
     public WebElement viewAllOrderTab;
+    @FindBy(linkText = "View all Products")
     public WebElement viewAllProductsTab;
+    @FindBy(linkText = "Order")
     public WebElement orderTab;
+
+    public WCommonArea(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
