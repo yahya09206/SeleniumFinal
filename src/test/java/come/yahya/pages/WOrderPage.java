5b43c34bdd899f55ba962ae2afdf507e5cbfd4ba
Yahya <yahyahussein91@gmail.com>
2024-01-15 13:32:51 -0800

    setting up page to process orders

--- src/test/java/come/yahya/pages/WOrderPage.java
+++ src/test/java/come/yahya/pages/WOrderPage.java
@@ -1,4 +1,28 @@
 package come.yahya.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class WOrderPage {
+
+    @FindBy(xpath = "//h2[normalize-space(.)='Order']")
+    public WebElement header;
+    @FindBy(id = "ctl00_Main_fmwOrder_ddlProduct")
+    public WebElement productDropdown;
+
+    @FindBy(id = "ctl00_Main_fmwOrder_txtQuantity")
+    public WebElement quantityBox;
+
+    @FindBy(id = "ctl00_Main_fmwOrder_txtUnitPrice")
+    public WebElement priceBox;
+
+    @FindBy(id = "ctl00_Main_fmwOrder_txtDiscount")
+    public WebElement discountBox;
+
+    @FindBy(id = "ctl00_Main_fmwOrder_txtTotal")
+    public WebElement totalBox;
+
+    @FindBy(css = "ctl00_Main_fmwOrder_ddlProduct")
+    public WebElement calculateButton;
+
 }
