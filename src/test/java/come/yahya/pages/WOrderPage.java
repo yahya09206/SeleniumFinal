657a4d925c585afe51c5065e7a91973b4a6be240
Yahya <yahyahussein91@gmail.com>
2024-01-15 13:40:49 -0800

    set up elements

--- src/test/java/come/yahya/pages/WOrderPage.java
+++ src/test/java/come/yahya/pages/WOrderPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class WOrderPage {
 
+    // Product Information
     @FindBy(xpath = "//h2[normalize-space(.)='Order']")
     public WebElement header;
     @FindBy(id = "ctl00_Main_fmwOrder_ddlProduct")
@@ -22,7 +23,37 @@ public class WOrderPage {
     @FindBy(id = "ctl00_Main_fmwOrder_txtTotal")
     public WebElement totalBox;
 
-    @FindBy(css = "ctl00_Main_fmwOrder_ddlProduct")
+    @FindBy(css = "input[type='submit'][value='Calculate']")
     public WebElement calculateButton;
+
+
+    // Address Info
+    @FindBy(id = "ctl00_MainContent_fmwOrder_txtName")
+    public WebElement customerNameBox;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_TextBox2")
+    public WebElement streetBox;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_TextBox3")
+    public WebElement cityBox;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_TextBox4")
+    public WebElement stateBox;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_TextBox5")
+    public WebElement zipBox;
+
+    // Payment Information
+    @FindBy(id = "ctl00_MainContent_fmwOrder_cardList_0")
+    public WebElement cardRadio;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_TextBox6")
+    public WebElement cardNumber;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_TextBox1")
+    public WebElement cardDate;
+
+    @FindBy(id = "ctl00_MainContent_fmwOrder_InsertButton")
+    public WebElement processButton;
 
 }
